6f68ca514445f982e9a4183efc253bca510c4b04
Dündar <78531321+Dundar-Demirtas@users.noreply.github.com>
2023-04-13 16:32:00 +0300

    Add files via upload
    e-commerce website design, individualized pages did not edited yet

---
+++ main.js
@@ -0,0 +1,14 @@
+const mnc1 = document.querySelector("#mp1").onclick = function () {
+    location.href ="mainp/mp1.html" };
+
+
+
+const mnc2 = document.querySelector("#mp2").onclick = function () {
+        location.href ="mainp/mp2.html" };
+
+
+
+
+const mnc3 = document.querySelector("#mp3").onclick = function () {
+            location.href ="mainp/mp3.html" };
+             /* main page 3 products ends*/
---
+++ mn.js
@@ -0,0 +1,39 @@
+
+
+             /* products part9 ends */
+
+
+const card1 = document.querySelector("#crd1").onclick = function () {
+    location.href ="products/products1.html" };
+
+const card2 = document.querySelector("#crd2").onclick = function () {
+        location.href ="products/products2.html" };
+
+const card3 = document.querySelector("#crd3").onclick = function () {
+            location.href ="products/products3.html" };
+
+const card4 = document.querySelector("#crd4").onclick = function () {
+location.href ="products/products4.html" };
+
+const card5 = document.querySelector("#crd5").onclick = function () {
+    location.href ="products/products5.html" };
+ 
+const card6 = document.querySelector("#crd6").onclick = function () {
+        location.href ="products/products6.html" };
+
+const card7 = document.querySelector("#crd7").onclick = function () {
+            location.href ="products/products7.html" };
+
+ const card8 = document.querySelector("#crd8").onclick = function () {
+                location.href ="products/products8.html" }
+
+ const card9 = document.querySelector("#crd9").onclick = function () {
+                    location.href ="products/products9.html" };
+
+
+                    /* products part9 ends */
+    
+                   
+
+
+                   
